2d92ec17015fa2320e87d6927044878ac5d9b04a
FREYSS <you@example.com>
2026-06-24 16:46:24 +0200

    feat: base des classes communes css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { Search } from './pages/search/search';
+import { Recipes } from './pages/recipes/recipes/recipes';
 
 export const routes: Routes = [
+  { path: '', component: Recipes }, // route par default
   { path: 'search', component: Search },
   {
     path: 'recipes',
--- src/app/pages/recipes/recipes/recipes.ts
+++ src/app/pages/recipes/recipes/recipes.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SearchField } from '../../../components/fields/search-field/search-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-recipes',
-  imports: [],
+  imports: [SearchField, MatIconModule, MatButtonModule],
   templateUrl: './recipes.html',
   styleUrl: './recipes.scss'
 })

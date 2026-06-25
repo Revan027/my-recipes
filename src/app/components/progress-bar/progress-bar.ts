384f4a8ff065f636c85cce6d65e69f02e292d54e
FREYSS <you@example.com>
2026-06-25 12:59:11 +0200

    feat: début intégration book de recette

---
+++ src/app/components/progress-bar/progress-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'app-progress-bar',
+  imports: [MatProgressBarModule],
+  templateUrl: './progress-bar.html',
+  styleUrl: './progress-bar.scss'
+})
+export class ProgressBar {}
--- src/app/pages/recipes/recipe/recipe.ts
+++ src/app/pages/recipes/recipe/recipe.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ProgressBar } from '../../../components/progress-bar/progress-bar';
 
 @Component({
   selector: 'app-recipe',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, ProgressBar],
   templateUrl: './recipe.html',
   styleUrl: './recipe.scss'
 })
